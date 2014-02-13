597fbe93226a3968fc12a39206f1f971d42be728
Olga Prilepova <oprilepova@ucdavis.edu>
2014-02-12 19:51:18 -0800

    works perfectly. full 2a

--- ps3/2.R
+++ ps3/2.R
@@ -1,13 +1,29 @@
 erlangmix <- function(r,nmix=NULL,qftn=NULL,const=NULL) {
-	returnVector <- r
+	returnVector <- c(r)
 	if (!is.null(const) && (!is.null(nmix) || !is.null(qftn))){  
 		stop("either const or both nmix and qftn have to be provided as arguments")
 	} else if (!is.null(const)){ #constant simulation case
-		lambda<-const/r
+		lambda<-r/const
 		returnVector<-c(returnVector,lambda)
-	} else if (is.null(nmix) || is.null(qftn)){ #const is null for sure
+	} else if (is.null(nmix) || nmix<1 || is.null(qftn)){ #const is null for sure
         stop("either const or both nmix and qftn have to be provided as arguments")
 	} else { #function simulation case
-
+		delta <- 1/(nmix+1) #nmix or nmix+1 here?
+		print(delta)
+		x <- delta
+		while (x<1){
+			point <- qftn(x) #point to find lambda for
+			x <- x + delta 
+			lambda<-r/point
+			returnVector<-c(returnVector,lambda)
+		}
 	}
+	returnVector
+}
+
+test <- function() {
+   # consider the density having the value 2t on (0,1); its quantile
+   # function is the sqrt function
+   qf <- function(q) sqrt(q)
+   print(erlangmix(500,250,qf))
 }
