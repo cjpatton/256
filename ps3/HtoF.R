3741324886e6df9fd8385720bd9831ea47d7fc8c
jhochen <jhochen@ucdavis.edu>
2014-02-17 19:35:57 -0800

    SO MEAN

--- ps3/HtoF.R
+++
@@ -1,9 +0,0 @@
-htof = function(hftn,t,lower){
-  density_val = c()
-  for(val in t)
-  {
-    density_val = c(density_val, hftn(val) * 
-          exp(-1*integrate(hftn,lower,val)$value))
-  }
-  return (density_val)
-}
