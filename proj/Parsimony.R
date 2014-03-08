fbae7323d6f0c771fc700c7bea1406846d91f70b
Christopher Patton <cjpatton@ucdavis.edu>
2014-03-07 17:07:10 -0800

    Completed prsm().

--- proj/Parsimony.R
+++ proj/Parsimony.R
@@ -17,30 +17,38 @@ ar2 <- function(y, x, cols)
 # random variable (in {0,1}). (Calls glm().)
 aiclogit <- function(y, x, cols)
 {
-  a = (glm(formula = y ~ ., data=subset(x,select=cols), family = binomial)
+  a = glm(formula = y ~ ., data=subset(x, select=cols), family = binomial)
   return (a$aic)
-  
 }
 
 # Reduce the parsimony of a data set for predicting the response variable 'y'. 
 # 'x' is an NxR matrix, where N is the number of samples and R the number of 
 # attributes per sample. Return a vector of column names. 
 #  TODO How to prevent over fitting when then the data set is small? 
-#  TODO prediction accuracy criterion for aiclogit().
-prsm <- function(y, x, k=0.1, predacc=ar2, crit="max", printdel=F) 
+prsm <- function(y, x, k=0.01, predacc=ar2, crit="min", printdel=F) 
 {
   orig_cols <- colnames(x)
   cols <- orig_cols 
-  pac <- predacc(y, x, cols) 
+  pac <- predacc(y, x, cols)
+  cat("full outcome = ", pac, "\n")
 
   for (col in orig_cols) 
   {
     new_cols <- setdiff(cols, col)
-    new_pac <- predacc(y, x, new_cols)        
-    if (crit == "max" & new_pac > pac | pac - new_pac < k) # ar2() case 
+    new_pac <- predacc(y, x, new_cols)
+    if (crit == "max" & (new_pac >= pac | new_pac >= (1-k)*pac)) # ar2() case 
     {
       cols <- new_cols
       pac <- new_pac
+      cat("deleted        ", col, "\n")
+      cat("new outcome  = ", pac, "\n")
+    }
+    else if ( crit == "min" & (new_pac <= pac | new_pac <= (1+k)*pac )) # aiclogit() case
+    {
+      cols <- new_cols
+      pac <- new_pac
+      cat("deleted        ", col, "\n")
+      cat("new outcome  = ", pac, "\n")
     }
   }
   return (cols)
@@ -48,5 +56,6 @@ prsm <- function(y, x, k=0.1, predacc=ar2, crit="max", printdel=F)
 
 # Testing, testing ... 
 df <- read.csv("pima.csv", header=T)
-parsimony <- prsm(df$age, subset(df, select=-c(age)), k=0.01)
+#parsimony <- prsm(df$age, subset(df, select=-c(age)), k=0.01, crit="max")
+parsimony <- prsm(df$class, subset(df, select=-c(class)), predacc=aiclogit, k=0.01)
 print(parsimony)
