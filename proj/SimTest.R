28142dbb11b33e29925d78c1e64bb8a4ff0e9fd2
Thomas Provan <tcprovan@ucdavis.edu>
2014-03-09 19:28:26 -0700

    SimTest.R should be finished now, working on request table. Being very unsatisfied with it

--- proj/SimTest.R
+++ proj/SimTest.R
@@ -14,7 +14,7 @@ genY <- function(X)
 
 
 
-SimTest <- function(n, k)
+SimTest <- function(n)
 {
 
 	X <- matrix(runif(n * 10), n)
@@ -23,20 +23,103 @@ SimTest <- function(n, k)
 
 	xFrame <- as.data.frame(X)
 
-	pred <- prsm(Y, xFrame, k=k, crit="max")
+	pred1 <- prsm(Y, xFrame, k=0.01, crit="max")
 
-	return(pred)
+	pred5 <- prsm(Y, xFrame, k=0.05, crit="max")
+
+
+	#print(summary(lm(Y ~ X)))
+
+	ret <- list(X, Y, pred1, pred5)
+
+	return(ret)
 }
 
-threeRuns <- function(n, k)
+threeRuns <- function(n)
+{
+	
+	print("Run 1")
+	oneRun(n)
+
+	print("Run 2")
+	oneRun(n)
+
+	print("Run 3")
+	oneRun(n)
+
+}
+
+oneRun <- function(n)
 {
 
-	print(SimTest(n, k))
-	print(SimTest(n, k))
-	print(SimTest(n, k))
+	res <- SimTest(n)
+	print(res[[3]])
+	print(res[[4]])
+
+	Y <- res[[2]]
+	X <- res[[1]]
+
+	prRes <- summary(lm(Y~X))["coefficients"][[1]][,4]
+	sigRes <- prRes[prRes < 0.05]
+
+	print(names(sigRes))
 
 }
 
+
+multiTest <- function(m, n)
+{
+
+	count1 <- rep(0, 10)
+	count5 <- rep(0, 10)
+	countSig <- rep(0, 10)
+
+	names(countSig) <- paste("X", 1:10, sep="")
+	
+	names(count1) <- paste("V", 1:10, sep="")
+	names(count5) <- paste("V", 1:10, sep="")
+
+	#print(count)
+
+	for(i in 1:m){
+
+		result <- SimTest(n)
+
+		for(i in result[[3]]){
+			count1[i] <- count1[i] + 1
+			#print(count[i])
+		}
+		for(i in result[[4]]){
+			count5[i] <- count5[i] + 1
+			#print(count[i])
+		}
+
+		Y <- result[[2]]
+		X <- result[[1]]
+
+		prRes <- summary(lm(Y~X))["coefficients"][[1]][,4]
+		sigRes <- prRes[prRes < 0.05]
+
+		for(i in names(sigRes))
+		{
+			if(i != "(Intercept)"){
+				countSig[i] <- countSig[i] + 1
+				#print(countSig[i])
+			}
+		}
+
+	}
+
+	propSig <- countSig / m
+	prop1 <- count1 / m
+	prop5 <- count5 / m
+
+	ret <- list(prop1, prop5, propSig)
+
+	names(ret) <- c("prsm(k=0.01)", "prsm(k=0.05)", "signif")
+
+	return(ret)
+}
 
 sigTest <- function(n)
 {
